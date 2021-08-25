c5c7792959d0df45d217db1dc05968293d07e89d
sephson <disutj@gmail.com>
2021-08-25 05:02:33 +0100

    'oauth'

---
+++ auth.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth2").Strategy;
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:5000/auth/google/callback",
+      passReqToCallback: true,
+    },
+    function (request, accessToken, refreshToken, profile, done) {
+      // User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      //   return done(err, user);
+      // }); ----> if you're creating a database
+
+      return done(err, profile);
+    }
+  )
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 const express = require("express");
 const app = express();
+require("./auth");
+const session = require('express-session');
+const passport = require("passport");
+
+function isLoggedIn(req, res, next) {
+  req.user ? next() : res.sendStatus(401);
+}
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/", (req, res) => {
-    res.send('<a href="/auth/google">Authenticate with google</a>')
+  res.send('<a href="/auth/google">Authenticate with google</a>');
 });
 
-app.get('/protected', (req, res)=>{
-    res.send('Helloo')
-})
+app.get("/auth/google", (req, res) => {
+  passport.authenticate("google", { scope: ["email", "profile"] });
+});
+
+app.get("/google/callback", (req, res) => {
+  passport.authenticate("google", {
+    successRedirect: "/protected",
+    failureRedirect: "/auth/failure",
+  });
+});
+
+app.get("/auth/failure", (req, res) => {
+  res.send("something went wrong");
+});
+
+app.get("/protected", isLoggedIn, (req, res) => {
+  res.send("Helloo");
+});
 
 app.listen(5000, () => console.log("Server running"));
